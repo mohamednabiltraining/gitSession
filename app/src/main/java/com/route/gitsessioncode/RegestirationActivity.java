8d2aed931a5c74c7e614e7e530f36a8f601be827
Mohamed <mohamed.adel201568@gmail.com>
2019-09-14 17:35:36 +0200

    registration UI added

---
+++ app/src/main/java/com/route/gitsessioncode/RegestirationActivity.java
@@ -0,0 +1,14 @@
+package com.route.gitsessioncode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegestirationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regestiration);
+    }
+}
